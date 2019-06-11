fb8609054dfcb219b04dee3f444523c81d6fccf3
Myriam <myriamb2010@yahoo.fr>
2019-06-11 11:11:01 +0200

    second commit

---
+++ wish-list.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function(){
+
+    var $wish1 = $('#wish1'),
+        $wish2 = $('#wish2'),
+        $wish3 = $('#wish3'),
+      
+        $envoi = $('#envoi'),
+        $reset = $('#rafraichir'),
+        $erreur = $('#erreur'),
+        $champ = $('.champ');
+
+});
